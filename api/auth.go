3b4351d561727bdfe59155f500a3a810806126c3
chad-bekmezian-snap <chad.bekmezian@snapone.com>
2025-04-13 20:43:58 -0600

    fix: uri

--- api/auth.go
+++ api/auth.go
@@ -6,5 +6,5 @@ import (
 )
 
 func AuthHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Auth!</h1>")
+	fmt.Fprintf(w, "<h1>Hello from Auth!: %s</h1>", r.RequestURI)
 }
